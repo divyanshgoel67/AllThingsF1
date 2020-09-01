e4d3988763df349b1f288241f50374c4807ef90b
divyansh.goel <divyansh.goel@olacabs.com>
2020-09-01 16:16:33 +0530

    Change default action bar for main activity

--- app/src/main/java/com/example/allthingsf1/activity/MainActivity.kt
+++ app/src/main/java/com/example/allthingsf1/activity/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.allthingsf1.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import com.example.allthingsf1.R
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    setTheme(R.style.AppTheme)
+    setTheme(R.style.AppTheme_CustomBarTheme)
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    // Change default action bar to custom bar
+    supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM;
+    supportActionBar?.setDisplayShowCustomEnabled(true)
+    supportActionBar?.setCustomView(R.layout.custom_toolbar);
   }
 }
