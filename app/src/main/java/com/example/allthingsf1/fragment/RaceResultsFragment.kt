7939b4d43c85d2be2c8340878354c0460283b078
divyansh.goel <divyansh.goel@olacabs.com>
2020-10-20 23:18:27 +0530

    Modify race results fragment and view model class

--- app/src/main/java/com/example/allthingsf1/fragment/RaceResultsFragment.kt
+++ app/src/main/java/com/example/allthingsf1/fragment/RaceResultsFragment.kt
@@ -1,12 +1,19 @@
 package com.example.allthingsf1.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.allthingsf1.R
+import com.example.allthingsf1.recyclerview.SeasonRaceResultsRecyclerViewAdapter
+import com.example.allthingsf1.viewmodel.RaceResultsViewModel
+import kotlinx.android.synthetic.main.fragment_race_results.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -14,13 +21,45 @@ import com.example.allthingsf1.R
 class RaceResultsFragment : Fragment() {
 
     private lateinit var fragmentView: View
+    private lateinit var seasonText: TextView
+    private lateinit var resultsRecyclerView: RecyclerView
+    private lateinit var adapter: SeasonRaceResultsRecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         fragmentView = inflater.inflate(R.layout.fragment_race_results, container, false)
+        initialiseViews()
+        initialiseRaceResultsRecyclerView()
+        var season = "2020"
+
+        var raceResultsViewModel: RaceResultsViewModel =
+            ViewModelProvider(this).get(RaceResultsViewModel::class.java)
+
+        raceResultsViewModel.seasonRaceResultsData.observe(viewLifecycleOwner, Observer {
+            adapter.updateRaceResultsList(it)
+        })
+
+        raceResultsViewModel.raceScheduleData.observe(viewLifecycleOwner, Observer {
+            adapter.updateRaceScheduleList(it)
+        })
+
         return fragmentView
+    }
+
+    private fun initialiseViews() {
+//        seasonText = fragmentView.season_text
+        val season = "2020"
+//        seasonText.text = "Season $season"
+        resultsRecyclerView = fragmentView.race_results_recycler_view
+    }
+
+    private fun initialiseRaceResultsRecyclerView() {
+        adapter = SeasonRaceResultsRecyclerViewAdapter(requireContext())
+        var layoutManager = LinearLayoutManager(requireContext())
+        resultsRecyclerView.adapter = adapter
+        resultsRecyclerView.layoutManager = layoutManager
     }
 
 }
--- app/src/main/java/com/example/allthingsf1/viewmodel/RaceResultsViewModel.kt
+++ app/src/main/java/com/example/allthingsf1/viewmodel/RaceResultsViewModel.kt
@@ -1,17 +1,40 @@
 package com.example.allthingsf1.viewmodel
 
-import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.allthingsf1.RepositoryManager
 import com.example.f1repository.RaceResultsRepository
+import com.example.f1repository.RaceScheduleRepository
 import com.example.models.raceresult.RaceResultsModel
+import com.example.models.raceschedule.ScheduleEntityModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RaceResultsViewModel(var season: String, var applicationContext: Context) : ViewModel() {
+class RaceResultsViewModel() : ViewModel() {
 
-    var repository: RaceResultsRepository = RaceResultsRepository(season, applicationContext)
+    @Inject lateinit var raceResultsRepo: RaceResultsRepository
 
-    var seasonRaceResultsData : LiveData<List<RaceResultsModel>> = repository.getCurrentSeasonRaceResults()
+    @Inject lateinit var raceScheduleRepository : RaceScheduleRepository
 
-    var resultsFailedNotifier: LiveData<String> = repository.getFailedResultsLiveData()
+    init {
+        RepositoryManager.instance.getRepositoryComponent().inject(this)
+    }
+
+    var raceScheduleData : LiveData<List<ScheduleEntityModel>> = raceScheduleRepository.raceScheduleData
+
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            val completedRaces = raceResultsRepo.getTotalCompletedRaces()
+            raceScheduleRepository.getSeasonRaceSchedule(completedRaces)
+        }
+    }
+
+    var seasonRaceResultsData : LiveData<List<RaceResultsModel>> = raceResultsRepo.getCurrentSeasonRaceResults()
+
+    var resultsFailedNotifier: LiveData<String> = raceResultsRepo.getFailedResultsLiveData()
 
 }
