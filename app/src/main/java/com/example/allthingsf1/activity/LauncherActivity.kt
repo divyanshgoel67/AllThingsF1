172be666995834593c9851917daa482c1cd321b4
divyansh.goel <divyansh.goel@olacabs.com>
2020-10-20 23:13:36 +0530

    Modify activity classes

--- app/src/main/java/com/example/allthingsf1/activity/LauncherActivity.kt
+++ app/src/main/java/com/example/allthingsf1/activity/LauncherActivity.kt
@@ -1,13 +1,34 @@
 package com.example.allthingsf1.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.allthingsf1.F1SharedPreference
 import com.example.allthingsf1.R
+import com.example.allthingsf1.RepositoryManager
+import com.example.f1repository.SeasonsListRepository
+import javax.inject.Inject
 
 class LauncherActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var seasonsListRepository: SeasonsListRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.LauncherActivityTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launcher)
+
+        RepositoryManager.instance.getStaticRepositoryComponent().inject(this)
+
+        seasonsListRepository.getCurrentSeason().observe(this, Observer {
+            F1SharedPreference.instance.setDisplaySeason(it.season)
+            openMainActivity()
+        })
+
+    }
+
+    private fun openMainActivity() {
+
     }
 }
--- app/src/main/java/com/example/allthingsf1/activity/MainActivity.kt
+++ app/src/main/java/com/example/allthingsf1/activity/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.allthingsf1.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -10,7 +10,10 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.allthingsf1.DaggerRepositoryComponent
 import com.example.allthingsf1.R
+import com.example.allthingsf1.RepositoryManager
+import com.example.f1repository.dagger.RepositoryModule
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +21,19 @@ class MainActivity : AppCompatActivity() {
   private lateinit var navController: NavController
   private lateinit var appBarConfig: AppBarConfiguration
   private lateinit var drawerLayout: DrawerLayout
+    private lateinit var displaySeason: String
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+      displaySeason = "2020"
+
+      // Initialize repository component
+      var repositoryComponent =
+          DaggerRepositoryComponent.builder().repositoryModule(RepositoryModule(displaySeason))
+              .build()
+      RepositoryManager.instance.updateRepositoryComponent(repositoryComponent)
 
     val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     navController = navHostFragment.navController
@@ -44,4 +56,14 @@ class MainActivity : AppCompatActivity() {
     return navController.navigateUp()
             || super.onSupportNavigateUp()
   }
+
+    fun changeDisplaySeason(season: String) {
+        updateRepositoryComponent(season)
+    }
+
+    private fun updateRepositoryComponent(season: String) {
+        var repositoryComponent =
+            DaggerRepositoryComponent.builder().repositoryModule(RepositoryModule(season)).build()
+        RepositoryManager.instance.updateRepositoryComponent(repositoryComponent)
+    }
 }
